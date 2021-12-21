f3735ed7bc5b8c890bae9ff0152243549ca1de7d
RobertoPantalogne <mr.ded2016@gmail.com>
2021-12-21 14:19:35 +0200

    Chat bot 2-ndt stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,10 +1,16 @@
 package ChatBot;
+import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args){
+        Scanner scamer = new Scanner(System.in);
         String bot_name = "Mehmet";
         int birth_year = 1992;
         System.out.println("Hello my name is " + bot_name);
         System.out.println("I was created in " + birth_year);
+        System.out.println("Please remind me your name ");
+        String scaner = scamer.nextLine();
+        System.out.println("What a great name you have " + scaner);
+
     }
 }
