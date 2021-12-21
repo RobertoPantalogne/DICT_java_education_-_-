f5b71f0dff86e4c512fc31c6a48b6856a20cd809
RobertoPantalogne <mr.ded2016@gmail.com>
2021-12-21 22:23:35 +0200

    Seventh stage Hangman

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,11 +1,8 @@
 package Hangman;
 import org.apache.groovy.parser.antlr4.util.StringUtils;
-
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-
-
 public class Hangman {
     public static void main(String[] args) {
         ArrayList guessed = new ArrayList();
@@ -20,11 +17,20 @@ public class Hangman {
         int i = 0;
         while (i < 8) {
             System.out.print("\nInput a letter:");
-            String input = scanner.nextLine();
+            String input = scanner.next();
+
+            if (!input.matches(".")) {
+                System.out.println("You should input a single letter");
+                continue;
+            }
+
+            if (!input.matches("[a-z]")) {
+                System.out.println("Please enter a lowercase English letter");
+                continue;
+            }
 
             if (guessed.contains(input.charAt(0))) {
-                System.out.println("No Improvements");
-                i++;
+                System.out.println("You've already guessed this letter");
                 continue;
             }
 
@@ -33,27 +39,22 @@ public class Hangman {
                 i++;
                 continue;
             }
-                guessed.add(input.charAt(0));
-
-                for (int z = 0; z < hidden_word.length(); z++) {
-                    if (w.charAt(z) == input.charAt(0)){
-                        hidden_word.setCharAt(z, input.charAt(0));
-
-
-                    }
+            guessed.add(input.charAt(0));
+            for (int z = 0; z < hidden_word.length(); z++) {
+                if (w.charAt(z) == input.charAt(0)){
+                    hidden_word.setCharAt(z, input.charAt(0));
                 }
-                System.out.println(hidden_word);
-
-                long lettersToGuess = StringUtils.countChar(hidden_word.toString(), '-');
-                if (lettersToGuess == 0) break;
-
             }
-
-            if (i == 8) {
-                System.out.println("You lost!");
-                return;
-            }
-            System.out.println("You guessed the word!");
-            System.out.println("You survived!");
+            System.out.println(hidden_word);
+            long lettersToGuess = StringUtils.countChar(hidden_word.toString(), '-');
+            if (lettersToGuess == 0) break;
         }
+        if (i == 8) {
+            System.out.println("You lost!");
+            return;
+        }
+
+        System.out.println("You guessed the word!");
+        System.out.println("You survived!");
     }
+}
