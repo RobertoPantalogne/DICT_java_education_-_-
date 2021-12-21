671b0a03879c442f05149f8e272ad8553a0747da
RobertoPantalogne <mr.ded2016@gmail.com>
2021-12-21 14:32:05 +0200

    Chat bot 3-rd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -11,6 +11,16 @@ public class ChatBot {
         System.out.println("Please remind me your name ");
         String scaner = scamer.nextLine();
         System.out.println("What a great name you have " + scaner);
+        System.out.println("Let guess your age");
+        System.out.println("Enter remainders of dividing your age by 3,5 and 7");
+        int remainder3 = scamer.nextInt();
+        int remainder5 = scamer.nextInt();
+        int remainder7 = scamer.nextInt();
+       int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that`s a great age to start programming!");
+
+
+
 
     }
 }
