39dffd25a550cd273dbbe91779c499ec5ccfb557
RobertoPantalogne <mr.ded2016@gmail.com>
2021-12-21 14:01:14 +0200

    Chat bot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,10 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args){
+        String bot_name = "Mehmet";
+        int birth_year = 1992;
+        System.out.println("Hello my name is " + bot_name);
+        System.out.println("I was created in " + birth_year);
+    }
+}
